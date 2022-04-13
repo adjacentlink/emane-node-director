633883a0be87cf1557a208ce9d176d44a68826d2
Eric Schreiber <eschreiber@adjacentlink.com>
2022-04-13 10:24:45 -0400

    Bump version to 0.2.2. Update copyright date. Remove patch file.

--- emane_node_director/nodetracker.py
+++ emane_node_director/nodetracker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2020 - Adjacent Link LLC, Bridgewater, New Jersey
+# Copyright (c) 2020-2022 - Adjacent Link LLC, Bridgewater, New Jersey
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
